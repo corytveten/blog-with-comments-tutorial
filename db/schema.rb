ca322e7a4b80ddb7f9f3ea52e856a81d9ab15151
corytveten <corytveten@gmail.com>
2020-08-30 09:10:58 -0700

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_024703) do
+ActiveRecord::Schema.define(version: 2020_08_30_151743) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
